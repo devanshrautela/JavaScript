cb716b401c561b011226e09cc4f28301f8664126
Devansh Rautela <maildevanshrautela@gmail.com>
2024-08-24 21:20:00 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,8 @@
+alert("This page will check wheather you input is a multiple of 5 or not");
+let num = prompt("Enter a Number");
+if(num % 5 === 0){
+    console.log("The Number is Multiple of 5");
+}
+else {
+    console.log("The Number is not a Multiple of 5");
+}
